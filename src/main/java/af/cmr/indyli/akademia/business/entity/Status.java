8fd2a1f7e0a8b885db0dd850d5ff989f49c18398
mohamed brayant <mohamed.brayant@indyli-services.com>
2024-02-27 11:03:44 +0100

    start

--- src/main/java/af/cmr/indyli/akademia/business/entity/Status.java
+++
@@ -1,9 +0,0 @@
-package af.cmr.indyli.akademia.business.entity;
-
-/**
- * This enum represents the status of a session. It defines various states that
- * an item can be in, such as open, closed, waiting, etc.
- */
-public enum Status {
-	OPEN, CLOSED, WAITING, ACTIVE, IN_PROGRESS, COMPLETE, CANCELLED
-}
