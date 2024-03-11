aa7773aa07175927cd519fb69a5f2c3161ea12b5
MathEyraud <eyraud-mathieu@outlook.fr>
2024-03-11 15:25:35 +0100

    Add entity Session

---
+++ src/main/java/af/cmr/indyli/akdemia/business/entity/Session.java
@@ -0,0 +1,173 @@
+package af.cmr.indyli.akdemia.business.entity;
+
+import java.util.Date;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "AKDEMIA_SESSION")
+public class Session {
+	
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID")
+    private Integer id;
+
+    @Column(name = "CODE")
+    private String code;
+
+    @Column(name = "DURATION")
+    private Double duration;
+
+    @Column(name = "PRICE")
+    private Double price;
+
+    @Column(name = "DESCRIPTION")
+    private String description;
+
+    @Column(name = "STATUS")
+    private String status;
+
+    @Column(name = "DATE")
+    private Date date;
+
+    @Column(name = "LOCATION")
+    private String location;
+
+    @Column(name = "SESSION_SCORE")
+    private Integer sessionScore;
+
+    @Column(name = "CREATION_DATE")
+    private Date creationDate;
+
+    @Column(name = "UPDATE_DATE")
+    private Date updateDate;
+
+    @ManyToOne
+    @JoinColumn(name = "ID_AKDEMIA_TRAINER", referencedColumnName = "ID", insertable = false, updatable = false)
+    private Trainer trainer;
+
+    @ManyToOne
+    @JoinColumn(name = "ID_AKDEMIA_TRAINING", referencedColumnName = "ID", insertable = false, updatable = false)
+    private Training training;
+
+    //
+	//
+	//
+	//
+	//
+	// ----------------------------- //
+	// ----- GETTERS & SETTERS ----- //
+	// ----------------------------- //
+    public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	public Double getDuration() {
+		return duration;
+	}
+
+	public void setDuration(Double duration) {
+		this.duration = duration;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	public String getLocation() {
+		return location;
+	}
+
+	public void setLocation(String location) {
+		this.location = location;
+	}
+
+	public Integer getSessionScore() {
+		return sessionScore;
+	}
+
+	public void setSessionScore(Integer sessionScore) {
+		this.sessionScore = sessionScore;
+	}
+
+	public Date getCreationDate() {
+		return creationDate;
+	}
+
+	public void setCreationDate(Date creationDate) {
+		this.creationDate = creationDate;
+	}
+
+	public Date getUpdateDate() {
+		return updateDate;
+	}
+
+	public void setUpdateDate(Date updateDate) {
+		this.updateDate = updateDate;
+	}
+
+	public Trainer getTrainer() {
+		return trainer;
+	}
+
+	public void setTrainer(Trainer trainer) {
+		this.trainer = trainer;
+	}
+
+	public Training getTraining() {
+		return training;
+	}
+
+	public void setTraining(Training training) {
+		this.training = training;
+	}
+    
+}
