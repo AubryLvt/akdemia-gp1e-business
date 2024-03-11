16ba66ec04bac77553fa0657e6d826a691a31e62
Fouad <bagfou@hotmail.com>
2024-03-11 16:12:51 +0100

    Add entity IntraSession

---
+++ src/main/java/af/cmr/indyli/akdemia/business/entity/IntraSession.java
@@ -0,0 +1,27 @@
+package af.cmr.indyli.akdemia.business.entity;
+
+import org.springframework.boot.autoconfigure.jms.JmsProperties.Listener.Session;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "akdemia_intra_session")
+public class IntraSession extends Session{
+	// serial Id à ajouter
+    
+	
+	 // Constructeur par défaut
+    public IntraSession() {
+        super(); // Appel du constructeur de la classe parente
+    }
+	 @OneToMany
+	 @JoinColumn(name = "ID_AKDEMIA_COMPANY", referencedColumnName 
+	 = "ID", insertable = false, updatable = false)
+	  private Company company;
+	    
+	    
+
+}
