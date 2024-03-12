bb2f0304da3b663db48b96697cdcb65920f60d7f
Pramps <aglprampart@gmail.com>
2024-03-12 11:31:00 +0100

    Bug Fix ValidationTest.java Ajout de la jonction entre la table AKDEMIA_TEST et AKDEMIA_VALIDATIONTEST Rajout de  @JoinColumn(name="ID_AKDEMIA_TEST") ligne 45

--- src/main/java/af/cmr/indyli/akdemia/business/entity/ValidationTest.java
+++ src/main/java/af/cmr/indyli/akdemia/business/entity/ValidationTest.java
@@ -5,6 +5,7 @@ import java.util.Date;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
@@ -40,8 +41,9 @@ public class ValidationTest implements IEntity {
 	@Column(name = "UPDATE_DATE")
 	private Date updateDate;
 
-	@ManyToOne
-	private Test test;
+    @ManyToOne
+    @JoinColumn(name="ID_AKDEMIA_TEST")
+    private Test test;
 	//
 	//
 	//
